ce363d73259269bc00aa655a2b0847c1a8e2d506
JonelBriones <jonel.c.briones@gmail.com>
2024-05-02 03:04:12 -0700

    changed img to svg

--- src/App.tsx
+++ src/App.tsx
@@ -45,11 +45,19 @@ function App() {
       <div className=" font-mono select-none relative mt-40">
         <div className="max-w-sm h-auto bg-white rounded-lg p-6 md:max-w-3xl  md:top-40   ">
           <div className="flex gap-6 items-center">
-            <img
-              src="../public/images/icon-star.svg"
-              alt="icon-star.svg"
+            <svg
               className="size-8"
-            />
+              xmlns="http://www.w3.org/2000/svg"
+              width="40"
+              height="41"
+              fill="none"
+              viewBox="0 0 40 41"
+            >
+              <path
+                fill="#AD28EB"
+                d="M37.5 20.5a2.467 2.467 0 0 1-1.64 2.344l-9.913 3.604-3.603 9.911a2.5 2.5 0 0 1-4.688 0l-3.604-9.922-9.911-3.593a2.5 2.5 0 0 1 0-4.688l9.921-3.604 3.594-9.911a2.5 2.5 0 0 1 4.688 0l3.604 9.921 9.911 3.594A2.467 2.467 0 0 1 37.5 20.5Z"
+              />
+            </svg>
             <h1 className="font-bold text-4xl">FAQs</h1>
           </div>
           {accordionData.map((accordion: Accordion, idx) => (
@@ -62,13 +70,33 @@ function App() {
             >
               <div className="flex place-items-center justify-between">
                 <h4 className="font-bold text-xl">{accordion.title}</h4>
-                <img
-                  src={`../public/images/icon-${
-                    accordion.show ? "minus" : "plus"
-                  }.svg`}
-                  alt="icon-star.svg"
-                  className="size-8"
-                />
+                {accordion?.show ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="31"
+                    fill="none"
+                    viewBox="0 0 30 31"
+                  >
+                    <path
+                      fill="#301534"
+                      d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="31"
+                    fill="none"
+                    viewBox="0 0 30 31"
+                  >
+                    <path
+                      fill="#AD28EB"
+                      d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"
+                    />
+                  </svg>
+                )}
               </div>
               <div
                 className={`grid overflow-hidden transition-all duration-200 ease-in-out ${
